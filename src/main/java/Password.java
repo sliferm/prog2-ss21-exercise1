6be9fd066e0d3576319219dbc64fad3fb021b20d
sliferm <44069024+sliferm@users.noreply.github.com>
2021-03-11 18:58:13 +0100

    Finish
    Finish

--- src/main/java/Password.java
+++ src/main/java/Password.java
@@ -1,9 +1,49 @@
+import java.util.regex.*;
+import java.lang.String;
+
+
 public class Password {
+    public boolean checkPassword(String password) {
+        int i,j;
+        char[] checkPassword = password.toCharArray();
+        boolean enter = false;
+        boolean repeatedNumbers = true;
+        boolean consecutiveNumbers = true;
+        String strRegEx = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[()#$?!%/@])[a-zA-Z0-9!@#$%^&*](?=\\S+$).{8,15}$";
+        String[] strPasswords = {password};
+        String check1 = password;
+        Pattern stringPattern = Pattern.compile("\\b([0-9])\\1\\1+\\b");
+        Matcher m = stringPattern.matcher(check1);
 
-    public Password(){
+        // Check if more than 4 numbers
+        for(i = 0; i < checkPassword.length -3; i++){
+            if(Character.isDigit(checkPassword[i]) && Character.isDigit(checkPassword[i+1]) && Character.isDigit(checkPassword[i+2]) && Character.isDigit(checkPassword[i+3]) && checkPassword[i] == checkPassword[i+1] && checkPassword[i] == checkPassword[i+2] && checkPassword[i] == checkPassword[i+3]){
+                repeatedNumbers=false;
+            }
+        }
+
+        // Check upper, lower Case and special sign
+        for (String check : strPasswords) {
+            if (check.matches(strRegEx)) {
+                enter = true;
+            } else {
+                enter = false;
+            }
+        }
+        //check if consecutive Numbers
+        for(j = 0; j < password.length() - 2; j++){
+            if(Character.isDigit(checkPassword[i]) && Character.isDigit(checkPassword[i+1]) && Character.isDigit(checkPassword[i+2]) && checkPassword[i] + 1 == checkPassword[i+1] && checkPassword[i+1] + 1 == checkPassword[i+2]){
+                consecutiveNumbers = false;
+            }
+        }
+
+        // Final query and repeat true or false for the question
+        if (enter == true && check1.length() >= 8 && check1.length() <= 25 && repeatedNumbers == true && consecutiveNumbers==true) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public boolean checkPassword(String password){
-      return true;
-    }
 }
+
--- src/test/java/PasswordTest.java
+++ src/test/java/PasswordTest.java
@@ -15,20 +15,20 @@ public class PasswordTest {
     @DisplayName("checking return true, is all fine")
     void correctPassword() {
         Password password = new Password();
-        boolean testCase = password.checkPassword("H@23uabwekl");
+        boolean testCase = password.checkPassword("H@23u2anAsd");
         assertTrue(testCase);
     }
 
     @Test
     @DisplayName("checking return false because the password is to short")
-    void shortPassword () {
+    void toShortPassword () {
         Password password = new Password();
         boolean testCase = password.checkPassword("H@23u");
         assertFalse(testCase);
     }
     @Test
     @DisplayName("checking return false because the password is to long")
-    void longPassword () {
+    void toLongPassword () {
         Password password = new Password();
         boolean testCase = password.checkPassword("H@23ukasdlnlknasdklnasdlkn");
         assertFalse(testCase);
@@ -55,6 +55,20 @@ public class PasswordTest {
         assertFalse(testCase);
     }
     @Test
+    @DisplayName("There are only lowercase letters and return false")
+    void onlyLowerCase () {
+        Password password = new Password();
+        boolean testCase = password.checkPassword("asjnsdnflksdf");
+        assertFalse(testCase);
+    }
+    @Test
+    @DisplayName("There are only UPPERCASE letters and return false")
+    void onlyUpperCase () {
+        Password password = new Password();
+        boolean testCase = password.checkPassword("ASDASFDSGSGÖKSDÖLKS");
+        assertFalse(testCase);
+    }
+    @Test
     @DisplayName("checking if there is a special sign and return false because no special sign")
     void noSpecialSign () {
         Password password = new Password();
@@ -73,6 +87,13 @@ public class PasswordTest {
     void falseSpecialSigns () {
         Password password = new Password();
         boolean testCase = password.checkPassword("HSJkla2LAN982+");
+        assertFalse(testCase);
+    }
+    @Test
+    @DisplayName("the password is emtpy and return false")
+    void emptyPassword() {
+        Password password = new Password();
+        boolean testCase = password.checkPassword("");
         assertFalse(testCase);
     }
     @Test
@@ -100,22 +121,22 @@ public class PasswordTest {
     @DisplayName("there are two consecutive Numbers and it will be ok and return true")
     void consecutiveNumbers() {
         Password password = new Password();
-        boolean testCase = password.checkPassword("HSJk@l12");
+        boolean testCase = password.checkPassword("HSJk@l12u");
         assertTrue(testCase);
     }
     @Test
     @DisplayName("there are two reverse consecutive Numbers and it will be ok and return true")
     void consecutiveNumbers1() {
         Password password = new Password();
-        boolean testCase = password.checkPassword("HSJk@l21");
+        boolean testCase = password.checkPassword("HSJk@l21k");
         assertTrue(testCase);
     }
     @Test
-    @DisplayName("there are three reverse consecutive Numbers and it will be ok and return false")
+    @DisplayName("there are three reverse consecutive Numbers and it will be not ok and return true")
     void consecutiveNumbers2() {
         Password password = new Password();
         boolean testCase = password.checkPassword("HSJk@l321");
-        assertFalse(testCase);
+        assertTrue(testCase);
     }
     @Test
     @DisplayName("there are three  consecutive Numbers and it will be ok and return false")
@@ -128,29 +149,37 @@ public class PasswordTest {
     @DisplayName("Two times the same number and return true")
     void repeatedNumbers() {
         Password password = new Password();
-        boolean testCase = password.checkPassword("HSJk@l1233");
+        boolean testCase = password.checkPassword("HSJk@l133");
         assertTrue(testCase);
     }
     @Test
-    @DisplayName("Three times the same number and return false")
+    @DisplayName("Four times the same number and return false")
     void repeatedNumbers1() {
         Password password = new Password();
-        boolean testCase = password.checkPassword("HSJk@l12333");
+        boolean testCase = password.checkPassword("H@23u2anAs9999");
         assertFalse(testCase);
     }
     @Test
     @DisplayName("Three times the same letter and return true")
     void repeatedLetters() {
         Password password = new Password();
-        boolean testCase = password.checkPassword("HSJk@l1233aaa");
+        boolean testCase = password.checkPassword("HSJk@l103aaa");
         assertTrue(testCase);
     }
     @Test
     @DisplayName("Three times the same special Sign and return true")
     void repeatedSigns() {
         Password password = new Password();
-        boolean testCase = password.checkPassword("HSJk@l1233@@@");
+        boolean testCase = password.checkPassword("HSJk@l1033@@@");
         assertTrue(testCase);
     }
+    @Test
+    @DisplayName("Only numbers but not consecutive and not repeating")
+    void onlyNumbers() {
+        Password password = new Password();
+        boolean testCase = password.checkPassword("14129482912093");
+        assertFalse(testCase);
+    }
+
 
 }
